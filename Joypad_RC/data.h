90d763ed0e6a6e0064c73f2952e0da21c3795881
老潘orz <wasdpkj@hotmail.com>
2016-02-23 11:22:48 +0800

    updata

--- Joypad_RC/data.h
+++ Joypad_RC/data.h
@@ -9,17 +9,6 @@ int16_t outBuf[8] = {
 
 boolean data_begin(int16_t * _channal_0, int16_t * _channal_1, int16_t * _channal_2, int16_t * _channal_3, int16_t * _channal_4, int16_t * _channal_5, int16_t * _channal_6, int16_t * _channal_7) {
   boolean _sta = true;
-  if (mode_protocol) {  //Robot
-    if (!Joy_sw_l) {
-      Joy_x = Joy_MID;
-      Joy_y = Joy_MID;
-      Joy1_x = Joy_MID;
-      Joy1_y = Joy_MID;
-    }
-  }
-  else {       //QuadCopter
-    if (!Joy_sw_l) Joy_y = Joy_MID - Joy_maximum;
-  }
 
   if (joypadUpdata()) {
     if (Joy_y < Joy_MID - Joy_maximum + 50) {
@@ -30,6 +19,18 @@ boolean data_begin(int16_t * _channal_0, int16_t * _channal_1, int16_t * _channa
 
       _sta = false;
     }
+  }
+
+  if (mode_protocol) {  //Robot
+    if (!Joy_sw_l) {
+      Joy_x = Joy_MID;
+      Joy_y = Joy_MID;
+      Joy1_x = Joy_MID;
+      Joy1_y = Joy_MID;
+    }
+  }
+  else {       //QuadCopter
+    if (!Joy_sw_l) Joy_y = Joy_MID - Joy_maximum;
   }
 
   int16_t * _i = _channal_0;
